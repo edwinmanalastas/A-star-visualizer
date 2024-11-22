a81f0ede5e105386e78ff3cb3716d73b01498c6c
Edwin M <edwinm.manalastas@gmail.com>
2024-11-22 03:27:50 -0700

    Initial Commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import pygame
+import math
+from queue import PriorityQueue
+
+WIDTH = 800
+WIN = pygame.display.set_mode((WIDTH,WIDTH))
+pygame.display.set_caption("A* Pathfinding Algorithm Visualizer")
+
+
